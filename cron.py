784964250b81e0e5e05fd676bbc9d5e0a4e67735
Camille SAURY <camillesaurypro@gmail.com>
2020-09-17 09:54:04 +0200

    Update cron.py

--- cron.py
+++ cron.py
@@ -2,6 +2,6 @@ from crontab import CronTab
 
 cron = CronTab(user=True)
 
-job = cron.new(command='python3 test.py')
+job = cron.new(command='python3 test.py --repo username/repo --token your_github_token --fb_username your_fb_username --fb_password your_fb_password --fb_group your_fb_group')
 job.minute.every(1)
 cron.write()
